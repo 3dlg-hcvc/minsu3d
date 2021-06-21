4f4e42989cb38925a5612bf919c2c7eb0d556c63
qiruiw <bitwqr@gmail.com>
2021-06-21 01:20:05 -0700

    update pointgroup_scannet.yaml

--- eval.py
+++ eval.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', '--config', type=str, default='conf/pointgroup_scannet.yaml', help='path to config file')
     parser.add_argument('-s', '--split', type=str, default='val', help='specify data split')
-    parser.add_argument('-t', '--task', type=str, default='', help='specify task: semantic | instance | alignment', required=True)
+    parser.add_argument('-t', '--task', type=str, default='', help='specify task: semantic | instance', required=True)
     args = parser.parse_args()
 
     base_cfg = OmegaConf.load('conf/path.yaml')
@@ -23,8 +23,6 @@ if __name__ == '__main__':
         evaluate_func = getattr(import_module('lib.evaluation'), 'evaluate_semantic')
     elif args.task == 'instance':
         evaluate_func = getattr(import_module('lib.evaluation'), 'evaluate_instance')
-    elif args.task == 'alignment':
-        evaluate_func = getattr(import_module('lib.evaluation'), 'evaluate_alignment')
         
     evaluate_func(cfg)
         
