14c531756f80e18cb644869c552bb0f50a38d049
qiruiw <bitwqr@gmail.com>
2021-09-29 14:46:32 -0700

    clean code in loss utils

--- lib/utils/loss.py
+++ lib/utils/loss.py
@@ -108,83 +108,9 @@ def compute_box_and_sem_cls_loss(loss_input, data_dict, loss_dict, mean_size_arr
     num_size_cluster = 18
     num_class = 18
     
-    pred_center, heading_scores, heading_residuals_normalized, heading_residuals, size_scores, size_residuals_normalized, size_residuals, sem_cls_scores, proposal_offsets = loss_input['proposal_pred_bboxes']
-    proposal_thres_mask = loss_input['proposal_thres_mask']
+    pred_center, heading_scores, heading_residuals_normalized, heading_residuals, size_scores, size_residuals_normalized, size_residuals, sem_cls_scores = loss_input['proposal_pred_bboxes']
+    proposal_offsets = loss_input['proposal_bbox_offsets']
     instance_offsets = data_dict['instance_offsets']
-    
-    ####### eval
-    # pred_center = end_points['center'] # num_proposal,3
-    pred_heading_class = torch.argmax(heading_scores, -1) # num_proposal
-    pred_heading_residual = torch.gather(heading_scores, 1, pred_heading_class.unsqueeze(-1)).squeeze(1) # num_proposal
-    pred_size_class = torch.argmax(size_scores, -1) # num_proposal
-    pred_size_residual = torch.gather(size_residuals, 1, pred_size_class.unsqueeze(-1).unsqueeze(-1).repeat(1,1,3)).squeeze(1) # num_proposal,3
-    # pred_sem_cls = torch.argmax(sem_cls_scores, -1) # num_proposal
-    # sem_cls_probs = softmax(sem_cls_scores.detach().cpu().numpy()) # num_proposal,18
-    # pred_sem_cls_prob = np.max(sem_cls_probs,-1) # B,num_proposal
-
-    num_proposal = pred_center.shape[0] 
-    # Since we operate in upright_depth coord for points, while util functions
-    # assume upright_camera coord.
-    pred_corners_3d_upright_camera = np.zeros((num_proposal, 8, 3))
-    # pred_center_upright_camera = flip_axis_to_camera(pred_center.detach().cpu().numpy())
-    pred_center_upright_camera = pred_center.detach().cpu().numpy()
-    for j in range(num_proposal):
-        heading_angle = DC.class2angle(pred_heading_class[j].detach().cpu().numpy(), pred_heading_residual[j].detach().cpu().numpy())
-        box_size = DC.class2size(int(pred_size_class[j].detach().cpu().numpy()), pred_size_residual[j].detach().cpu().numpy())
-        corners_3d_upright_camera = get_3d_box(pred_center_upright_camera[j,:], box_size, heading_angle)
-        pred_corners_3d_upright_camera[j] = corners_3d_upright_camera
-    
-    # iou between crop_bbox and pred_bbox
-    proposal_crop_bboxes, _ = loss_input['proposal_crop_bboxes']
-    # dist1, ind1, dist2, ind2 = nn_distance_stack(proposal_crop_bboxes[:, 0:3], data_dict['center_label'])
-    proposal_crop_bboxes = proposal_crop_bboxes.detach().cpu().numpy() # (nProposals, center+size+heading+label)
-    proposal_crop_bboxes = get_3d_box_batch(proposal_crop_bboxes[:, 0:3], proposal_crop_bboxes[:, 3:6], proposal_crop_bboxes[:, 6]) # (nProposals, 8, 3)
-    assert proposal_crop_bboxes.shape == pred_corners_3d_upright_camera.shape
-    ious = get_aabb3d_iou_batch(proposal_crop_bboxes, pred_corners_3d_upright_camera)
-    loss_dict['pred_crop_bbox_iou'] = (ious.mean(), num_proposal)
-    
-    # gt bbox
-    center_label = data_dict['center_label']
-    heading_class_label = data_dict['heading_class_label']
-    heading_residual_label = data_dict['heading_residual_label']
-    size_class_label = data_dict['size_class_label']
-    size_residual_label = data_dict['size_residual_label']
-    num_instance = center_label.shape[0] 
-    
-    gt_corners_3d_upright_camera = np.zeros((num_instance, 8, 3))
-    gt_center_upright_camera = center_label.detach().cpu().numpy()
-    for j in range(num_instance):
-        heading_angle = DC.class2angle(heading_class_label[j].detach().cpu().numpy(), heading_residual_label[j].detach().cpu().numpy())
-        box_size = DC.class2size(int(size_class_label[j].detach().cpu().numpy()), size_residual_label[j].detach().cpu().numpy())
-        corners_3d_upright_camera = get_3d_box(gt_center_upright_camera[j,:], box_size, heading_angle)
-        gt_corners_3d_upright_camera[j] = corners_3d_upright_camera
-    
-    # gt_ious = get_aabb3d_iou_batch(proposal_bbox, gt_corners_3d_upright_camera[ind1.cpu().numpy(),:,:])
-    # iou between crop_bbox and gt_bbox
-    crop_bbox_ious = np.zeros(num_proposal)
-    for b in range(batch_size):
-        pred_batch_start, pred_batch_end = proposal_offsets[b].cpu().numpy(), proposal_offsets[b+1].cpu().numpy()
-        pred_num = pred_batch_end - pred_batch_start # N
-        gt_batch_start, gt_batch_end = instance_offsets[b].cpu().numpy(), instance_offsets[b+1].cpu().numpy()
-        gt_num = gt_batch_end - gt_batch_start # M
-        
-        for i in range(pred_num):
-            crop_bbox_iou = get_aabb3d_iou_batch(np.tile(proposal_crop_bboxes[pred_batch_start+i], (gt_num, 1, 1)), gt_corners_3d_upright_camera[gt_batch_start:gt_batch_end])
-            crop_bbox_ious[pred_batch_start+i] = np.max(crop_bbox_iou)
-    loss_dict['crop_bbox_iou'] = (crop_bbox_ious.mean(), num_proposal)
-    
-    pred_bbox_ious = np.zeros(num_proposal)
-    for b in range(batch_size):
-        pred_batch_start, pred_batch_end = proposal_offsets[b].cpu().numpy(), proposal_offsets[b+1].cpu().numpy()
-        pred_num = pred_batch_end - pred_batch_start # N
-        gt_batch_start, gt_batch_end = instance_offsets[b].cpu().numpy(), instance_offsets[b+1].cpu().numpy()
-        gt_num = gt_batch_end - gt_batch_start # M
-        
-        for i in range(pred_num):
-            pred_bbox_iou = get_aabb3d_iou_batch(np.tile(pred_corners_3d_upright_camera[pred_batch_start+i], (gt_num, 1, 1)), gt_corners_3d_upright_camera[gt_batch_start:gt_batch_end])
-            pred_bbox_ious[pred_batch_start+i] = np.max(pred_bbox_iou)
-    loss_dict['pred_bbox_iou'] = (pred_bbox_ious.mean(), num_proposal)
-    ######## end eval
     
     center_loss = torch.tensor(0.).cuda()
     heading_class_loss = torch.tensor(0.).cuda()
@@ -193,21 +119,17 @@ def compute_box_and_sem_cls_loss(loss_input, data_dict, loss_dict, mean_size_arr
     size_reg_loss = torch.tensor(0.).cuda()
     sem_cls_loss = torch.tensor(0.).cuda()
 
-    # object_assignment = data_dict['object_assignment']
-    # batch_size = object_assignment.shape[0]
-    batch_size = len(proposal_offsets) - 1
     for b in range(batch_size):
         pred_batch_start, pred_batch_end = proposal_offsets[b], proposal_offsets[b+1]
         pred_num = pred_batch_end - pred_batch_start # N
         gt_batch_start, gt_batch_end = instance_offsets[b], instance_offsets[b+1]
         gt_num = gt_batch_end - gt_batch_start # M
         # Compute center loss
-        # pred_center = data_dict['center']
+
         pred_center_batch = pred_center[pred_batch_start:pred_batch_end]
         gt_center_batch = data_dict['center_label'][gt_batch_start:gt_batch_end]
         dist1, ind1, dist2, ind2 = nn_distance_stack(pred_center_batch, gt_center_batch) # dist1: (N,), dist2: (M,)
         object_assignment = ind1 # N
-        # box_label_mask = data_dict['box_label_mask']
         # objectness_label = data_dict['objectness_label'].float()
         centroid_reg_loss1 = torch.sum(dist1)/(pred_num+1e-6)
         centroid_reg_loss2 = torch.sum(dist2)/(gt_num+1e-6)
@@ -262,13 +184,6 @@ def compute_box_and_sem_cls_loss(loss_input, data_dict, loss_dict, mean_size_arr
         sem_cls_loss += criterion_sem_cls(sem_cls_scores_batch, sem_cls_label_batch) # (N,)
         # sem_cls_loss = torch.sum(sem_cls_loss * objectness_label)/(torch.sum(objectness_label)+1e-6)
         
-        # center_loss_total += center_loss
-        # heading_class_loss_total += heading_class_loss
-        # heading_residual_normalized_loss_total += heading_residual_normalized_loss
-        # size_class_loss_total += size_class_loss
-        # size_residual_normalized_loss_total += size_residual_normalized_loss
-        # sem_cls_loss_total += sem_cls_loss
-        
     loss_dict['center_loss'] = (center_loss/batch_size, batch_size)
     loss_dict['heading_cls_loss'] = (heading_class_loss/batch_size, batch_size)
     loss_dict['heading_reg_loss'] = (heading_reg_loss/batch_size, batch_size)
@@ -278,6 +193,5 @@ def compute_box_and_sem_cls_loss(loss_input, data_dict, loss_dict, mean_size_arr
     
     bbox_loss = center_loss + 0.1*heading_class_loss + heading_reg_loss + 0.1*size_class_loss + size_reg_loss
     loss_dict['bbox_loss'] = (bbox_loss/batch_size, batch_size)
-    # import pdb; pdb.set_trace()
 
     return loss_dict['bbox_loss'][0]
