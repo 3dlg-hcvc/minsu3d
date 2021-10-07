30f9b82c6a5973ceef924c008b6dadb956cc1c61
qiruiw <bitwqr@gmail.com>
2021-10-07 15:46:47 -0700

    add object detection evaluation script

--- lib/evaluation/__init__.py
+++ lib/evaluation/__init__.py
@@ -1,2 +1,3 @@
 from .semantic_segmentation import evaluate_semantic
 from .instance_segmentation import evaluate_instance
+from .object_detection import evaluate_detection
---
+++ lib/evaluation/object_detection.py
@@ -0,0 +1,397 @@
+import os, sys
+from glob import glob
+from importlib import import_module
+from multiprocessing import Pool
+
+import torch
+import numpy as np
+
+from lib.utils.log import Logger
+from lib.utils.bbox import box3d_iou
+from data.scannet.model_util_scannet import ScannetDatasetConfig
+
+# ---------- Label info ---------- #
+DC = None
+CFG = None
+
+# ---------- Evaluation params ---------- #
+AP_IOU_THRESHOLDS = [0.25, 0.5]
+
+
+""" 
+    Generic Code for Object Detection Evaluation
+    From: https://github.com/facebookresearch/votenet/blob/master/utils/eval_det.py
+    Input:
+    For each class:
+        For each image:
+            Predictions: box, score
+            Groundtruths: box
+    
+    Output:
+    For each class:
+        precision-recal and average precision
+    
+    Author: Charles R. Qi
+    
+    Ref: https://raw.githubusercontent.com/rbgirshick/py-faster-rcnn/master/lib/datasets/voc_eval.py
+"""
+
+def voc_ap(rec, prec, use_07_metric=False):
+    """ ap = voc_ap(rec, prec, [use_07_metric])
+    Compute VOC AP given precision and recall.
+    If use_07_metric is true, uses the
+    VOC 07 11 point method (default:False).
+    """
+    if use_07_metric:
+        # 11 point metric
+        ap = 0.
+        for t in np.arange(0., 1.1, 0.1):
+            if np.sum(rec >= t) == 0:
+                p = 0
+            else:
+                p = np.max(prec[rec >= t])
+            ap = ap + p / 11.
+    else:
+        # correct AP calculation 
+        # first append sentinel values at the end
+        mrec = np.concatenate(([0.], rec, [1.]))
+        mpre = np.concatenate(([0.], prec, [0.]))
+
+        # compute the precision envelope
+        for i in range(mpre.size - 1, 0, -1):
+            mpre[i - 1] = np.maximum(mpre[i - 1], mpre[i])
+
+        # to calculate area under PR curve, look for points where X axis (recall) changes value
+        i = np.where(mrec[1:] != mrec[:-1])[0]
+
+        # and sum (\Delta recall) * prec
+        ap = np.sum((mrec[i + 1] - mrec[i]) * mpre[i + 1])
+    return ap
+
+
+def calc_iou(box_a, box_b):
+    """Computes IoU of two axis aligned bboxes.
+    Args:
+        box_a, box_b: 6D of center and lengths        
+    Returns:
+        iou
+    """
+    max_a = box_a[0:3] + box_a[3:6]/2
+    max_b = box_b[0:3] + box_b[3:6]/2    
+    min_max = np.array([max_a, max_b]).min(0)
+        
+    min_a = box_a[0:3] - box_a[3:6]/2
+    min_b = box_b[0:3] - box_b[3:6]/2
+    max_min = np.array([min_a, min_b]).max(0)
+    if not ((min_max > max_min).all()):
+        return 0.0
+
+    intersection = (min_max - max_min).prod()
+    vol_a = box_a[3:6].prod()
+    vol_b = box_b[3:6].prod()
+    union = vol_a + vol_b - intersection
+    return 1.0*intersection / union
+
+
+def get_iou(bb1, bb2):
+    """ Compute IoU of two bounding boxes.
+        ** Define your bod IoU function HERE **
+    """
+    iou3d = calc_iou(bb1, bb2)
+    return iou3d
+
+from lib.utils.bbox import box3d_iou
+def get_iou_obb(bb1,bb2):
+    iou3d = box3d_iou(bb1,bb2)
+    return iou3d
+
+def get_iou_main(get_iou_func, args):
+    return get_iou_func(*args)
+
+def eval_det_cls(pred, gt, ovthresh=0.25, use_07_metric=False, get_iou_func=get_iou):
+    """ Generic functions to compute precision/recall for object detection
+        for a single class.
+        Input:
+            pred: map of {img_id: [(bbox, score)]} where bbox is numpy array
+            gt: map of {img_id: [bbox]}
+            ovthresh: scalar, iou threshold
+            use_07_metric: bool, if True use VOC07 11 point method
+        Output:
+            rec: numpy array of length nd
+            prec: numpy array of length nd
+            ap: scalar, average precision
+    """
+    # construct gt objects
+    class_recs = {} # {img_id: {'bbox': bbox list, 'det': matched list}}
+    npos = 0
+    for img_id in gt.keys():
+        bbox = np.array(gt[img_id])
+        det = [False] * len(bbox)
+        npos += len(bbox)
+        class_recs[img_id] = {'bbox': bbox, 'det': det}
+    # pad empty list to all other imgids
+    for img_id in pred.keys():
+        if img_id not in gt:
+            class_recs[img_id] = {'bbox': np.array([]), 'det': []}
+
+    # construct dets
+    image_ids = []
+    confidence = []
+    BB = []
+    for img_id in pred.keys():
+        for box,score in pred[img_id]:
+            image_ids.append(img_id)
+            confidence.append(score)
+            BB.append(box)
+    confidence = np.array(confidence)
+    BB = np.array(BB) # (nd,4 or 8,3 or 6)
+
+    # sort by confidence
+    sorted_ind = np.argsort(-confidence)
+    sorted_scores = np.sort(-confidence)
+    BB = BB[sorted_ind, ...]
+    image_ids = [image_ids[x] for x in sorted_ind]
+
+    # go down dets and mark TPs and FPs
+    nd = len(image_ids)
+    tp = np.zeros(nd)
+    fp = np.zeros(nd)
+    for d in range(nd):
+        #if d%100==0: print(d)
+        R = class_recs[image_ids[d]]
+        bb = BB[d,...].astype(float)
+        ovmax = -np.inf
+        BBGT = R['bbox'].astype(float)
+
+        if BBGT.size > 0:
+            # compute overlaps
+            for j in range(BBGT.shape[0]):
+                iou = get_iou_main(get_iou_func, (bb, BBGT[j,...]))
+                if iou > ovmax:
+                    ovmax = iou
+                    jmax = j
+
+        #print d, ovmax
+        if ovmax > ovthresh:
+            if not R['det'][jmax]:
+                tp[d] = 1.
+                R['det'][jmax] = 1
+            else:
+                fp[d] = 1.
+        else:
+            fp[d] = 1.
+
+    # compute precision recall
+    fp = np.cumsum(fp)
+    tp = np.cumsum(tp)
+    rec = tp / float(npos + 1e-8)
+    # print('NPOS: ', npos)
+    # avoid divide by zero in case the first detection matches a difficult
+    # ground truth
+    prec = tp / np.maximum(tp + fp, np.finfo(np.float64).eps)
+    ap = voc_ap(rec, prec, use_07_metric)
+
+    return rec, prec, ap
+
+def eval_det_cls_wrapper(arguments):
+    pred, gt, ovthresh, use_07_metric, get_iou_func = arguments
+    rec, prec, ap = eval_det_cls(pred, gt, ovthresh, use_07_metric, get_iou_func)
+    return (rec, prec, ap)
+
+def eval_det(pred_all, gt_all, ovthresh=0.25, use_07_metric=False, get_iou_func=get_iou):
+    """ Generic functions to compute precision/recall for object detection
+        for multiple classes.
+        Input:
+            pred_all: map of {img_id: [(classname, bbox, score)]}
+            gt_all: map of {img_id: [(classname, bbox)]}
+            ovthresh: scalar, iou threshold
+            use_07_metric: bool, if true use VOC07 11 point method
+        Output:
+            rec: {classname: rec}
+            prec: {classname: prec_all}
+            ap: {classname: scalar}
+    """
+    pred = {} # map {classname: pred}
+    gt = {} # map {classname: gt}
+    for img_id in pred_all.keys():
+        for classname, bbox, score in pred_all[img_id]:
+            if classname not in pred: pred[classname] = {}
+            if img_id not in pred[classname]:
+                pred[classname][img_id] = []
+            if classname not in gt: gt[classname] = {}
+            if img_id not in gt[classname]:
+                gt[classname][img_id] = []
+            pred[classname][img_id].append((bbox,score))
+    for img_id in gt_all.keys():
+        for classname, bbox in gt_all[img_id]:
+            if classname not in gt: gt[classname] = {}
+            if img_id not in gt[classname]:
+                gt[classname][img_id] = []
+            gt[classname][img_id].append(bbox)
+
+    rec = {}
+    prec = {}
+    ap = {}
+    for classname in gt.keys():
+        print('Computing AP for class: ', classname)
+        rec[classname], prec[classname], ap[classname] = eval_det_cls(pred[classname], gt[classname], ovthresh, use_07_metric, get_iou_func)
+        print(classname, ap[classname])
+    
+    return rec, prec, ap 
+
+def eval_det_multiprocessing(pred_all, gt_all, ovthresh=0.25, use_07_metric=False, get_iou_func=get_iou):
+    """ Generic functions to compute precision/recall for object detection
+        for multiple classes.
+        Input:
+            pred_all: map of {img_id: [(classname, bbox, score)]}
+            gt_all: map of {img_id: [(classname, bbox)]}
+            ovthresh: scalar, iou threshold
+            use_07_metric: bool, if true use VOC07 11 point method
+        Output:
+            rec: {classname: rec}
+            prec: {classname: prec_all}
+            ap: {classname: scalar}
+    """
+    pred = {} # map {classname: pred}
+    gt = {} # map {classname: gt}
+    for img_id in pred_all.keys():
+        for classname, bbox, score in pred_all[img_id]:
+            if classname not in pred: pred[classname] = {}
+            if img_id not in pred[classname]:
+                pred[classname][img_id] = []
+            if classname not in gt: gt[classname] = {}
+            if img_id not in gt[classname]:
+                gt[classname][img_id] = []
+            pred[classname][img_id].append((bbox,score))
+    for img_id in gt_all.keys():
+        for classname, bbox in gt_all[img_id]:
+            if classname not in gt: gt[classname] = {}
+            if img_id not in gt[classname]:
+                gt[classname][img_id] = []
+            gt[classname][img_id].append(bbox)
+
+    rec = {}
+    prec = {}
+    ap = {}
+    p = Pool(processes=10)
+    ret_values = p.map(eval_det_cls_wrapper, [(pred[classname], gt[classname], ovthresh, use_07_metric, get_iou_func) for classname in gt.keys() if classname in pred])
+    p.close()
+    for i, classname in enumerate(gt.keys()):
+        if classname in pred:
+            rec[classname], prec[classname], ap[classname] = ret_values[i]
+        else:
+            rec[classname] = 0
+            prec[classname] = 0
+            ap[classname] = 0
+        print(classname, ap[classname])
+    
+    return rec, prec, ap 
+
+
+class APCalculator(object):
+    ''' Calculating Average Precision '''
+    def __init__(self, ap_iou_thresh=0.25, class2type_map=None):
+        """
+        Args:
+            ap_iou_thresh: float between 0 and 1.0
+                IoU threshold to judge whether a prediction is positive.
+            class2type_map: [optional] dict {class_int:class_name}
+        """
+        self.ap_iou_thresh = ap_iou_thresh
+        self.class2type_map = class2type_map
+        self.reset()
+        
+    def step(self, batch_pred_map_cls, batch_gt_map_cls):
+        """ Accumulate one batch of prediction and groundtruth.
+        
+        Args:
+            batch_pred_map_cls: a list of lists [[(pred_cls, pred_box_params, score),...],...]
+            batch_gt_map_cls: a list of lists [[(gt_cls, gt_box_params),...],...]
+                should have the same length with batch_pred_map_cls (batch_size)
+        """
+        
+        batch_size = len(batch_pred_map_cls)
+        assert(batch_size == len(batch_gt_map_cls))
+        for i in range(batch_size):
+            self.gt_map_cls[self.scan_cnt] = batch_gt_map_cls[i] 
+            self.pred_map_cls[self.scan_cnt] = batch_pred_map_cls[i] 
+            self.scan_cnt += 1
+    
+    def compute_metrics(self):
+        """ Use accumulated predictions and groundtruths to compute Average Precision.
+        """
+        rec, prec, ap = eval_det_multiprocessing(self.pred_map_cls, self.gt_map_cls, ovthresh=self.ap_iou_thresh, get_iou_func=get_iou_obb)
+        ret_dict = {} 
+        for key in sorted(ap.keys()):
+            clsname = self.class2type_map[key] if self.class2type_map else str(key)
+            ret_dict['%s Average Precision'%(clsname)] = ap[key]
+        ret_dict['mAP'] = np.mean(list(ap.values()))
+        rec_list = []
+        for key in sorted(ap.keys()):
+            clsname = self.class2type_map[key] if self.class2type_map else str(key)
+            try:
+                ret_dict['%s Recall'%(clsname)] = rec[key][-1]
+                rec_list.append(rec[key][-1])
+            except:
+                ret_dict['%s Recall'%(clsname)] = 0
+                rec_list.append(0)
+        ret_dict['AR'] = np.mean(rec_list)
+        return ret_dict
+
+    def reset(self):
+        self.gt_map_cls = {} # {scan_id: [(classname, bbox)]}
+        self.pred_map_cls = {} # {scan_id: [(classname, bbox, score)]}
+        self.scan_cnt = 0
+
+
+def evaluate(pred_files, logger):
+    logger.debug(f'evaluating {len(pred_files)} scenes...')
+    
+    AP_CALCULATOR_LIST = [APCalculator(iou_thresh, DC.class2type) for iou_thresh in AP_IOU_THRESHOLDS]
+    batch_pred_map_cls = []
+    batch_gt_map_cls = []
+    for i in range(len(pred_files)):
+        bbox_data = torch.load(pred_files[i])
+        pred_bbox = bbox_data["pred_bbox"]
+        pred_sem_cls = bbox_data["pred_sem_cls"]
+        pred_obj_prob = bbox_data["pred_obj_prob"]
+        gt_bbox = bbox_data["gt_bbox"]
+        gt_bbox_label = bbox_data["gt_bbox_label"][0]
+        gt_sem_cls = bbox_data["gt_sem_cls"][0]
+        
+        if CFG.evaluation.detection.per_class_proposal:
+            cur_list = []
+            for ii in range(DC.num_class):
+                cur_list += [(ii, pred_bbox[j], pred_obj_prob[j]) for j in range(len(pred_bbox)) if pred_sem_cls[j]==ii]
+            batch_pred_map_cls.append(cur_list)
+        else:
+            batch_pred_map_cls.append([(pred_sem_cls[j], pred_bbox[j], pred_obj_prob[j]) for j in range(len(pred_bbox))])
+        
+        batch_gt_map_cls.append([(gt_sem_cls[j], gt_bbox[j]) for j in range(len(gt_bbox)) if gt_bbox_label[j]==1])
+
+        logger.debug(f"\rscenes processed: {i+1}")
+
+    for ap_calculator in AP_CALCULATOR_LIST:
+        ap_calculator.step(batch_pred_map_cls, batch_gt_map_cls)
+
+    for i, ap_calculator in enumerate(AP_CALCULATOR_LIST):
+        logger.info("-"*10 + " iou_thresh: %f "%(AP_IOU_THRESHOLDS[i]) + "-"*10)
+        metrics_dict = ap_calculator.compute_metrics()
+        for key in metrics_dict:
+            logger.info("eval %s: %f"%(key, metrics_dict[key]))
+        logger.info("\n")
+
+
+def evaluate_detection(cfg):
+    logger = Logger.from_evaluation(cfg)
+    
+    global CFG
+    global DC
+    CFG = cfg
+    DC = ScannetDatasetConfig(cfg)
+    
+    pred_path = os.path.join(cfg.OUTPUT_PATH, cfg.general.dataset, cfg.general.model, cfg.evaluation.use_model, "test", cfg.data.split, 'detection')
+    pred_files = sorted(glob(os.path.join(pred_path, '*.pth')))
+
+    # evaluate
+    evaluate(pred_files, logger)
