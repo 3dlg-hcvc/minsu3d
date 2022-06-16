74e353d90680e487eaf445e2ca95f708f0cc0d1b
eamonn-zh <eamonn0223@gmail.com>
2022-06-15 17:32:03 -0700

    update preprocessing code

---
+++ data/scannet/prepare_all_data.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+PTH_SCRIPT=prepare_one_scan_pth.py
+GT_SCRIPT=prepare_one_scan_gt.py
+TRAIN_IDS=meta_data/scannetv2_train.txt
+VAL_IDS=meta_data/scannetv2_val.txt
+CONFIG_PATH=../../conf/path.yaml
+PARALLEL_N=12
+
+echo "===================================="
+echo "Processing train split ..."
+echo "===================================="
+
+parallel -j $PARALLEL_N --bar "python $PTH_SCRIPT \
+    --id {1} \
+    --split train \
+    --cfg $CONFIG_PATH" :::: $TRAIN_IDS
+
+
+echo "===================================="
+echo "Processing val split ..."
+echo "===================================="
+
+parallel -j $PARALLEL_N --bar "python $PTH_SCRIPT \
+    --id {1} \
+    --split val \
+    --cfg $CONFIG_PATH" :::: $VAL_IDS
+
+
+echo "===================================="
+echo "Processing val split groud truth ..."
+echo "===================================="
+
+parallel -j $PARALLEL_N --bar "python $GT_SCRIPT \
+    --id {1} \
+    --split val \
+    --cfg $CONFIG_PATH" :::: $VAL_IDS
--- data/scannet/prepare_one_scan_pth.py
+++ data/scannet/prepare_one_scan_pth.py
@@ -2,10 +2,9 @@
 REFERENCE TO https://github.com/facebookresearch/votenet/blob/master/scannet/load_scannet_data.py
 '''
 
-import glob, plyfile, json, argparse, os
+import json, argparse, os
 from omegaconf import OmegaConf
 import numpy as np
-import multiprocessing as mp
 import torch
 import scannet_utils
 from plyfile import PlyData
@@ -55,7 +54,7 @@ def align_mesh_vertices(mesh, axis_align_matrix):
 def read_label_file(label_file):
     with open(label_file, 'rb') as f:
         plydata = PlyData.read(f)
-        sem_labels = np.array(plydata['vertex']['label']) #nyu40 
+        sem_labels = np.array(plydata['vertex']['label']) #nyu40
         # raw_labels[raw_labels > 40] = 40 # HACK !! # 0: unannotated
         # sem_labels = remapper[raw_labels]
     return sem_labels
@@ -75,9 +74,9 @@ def read_agg_file(agg_file):
                 # print('ignore wall, floor or ceiling')
                 continue
             else:
-                objectId2segs[objectId] = segs.copy()
+                objectId2segs[objectId] = segs
                 if label in label2segs:
-                    label2segs[label].extend(segs.copy())
+                    label2segs[label].extend(segs)
                 else:
                     label2segs[label] = segs.copy()
                 # objectId += 1
@@ -122,7 +121,7 @@ def get_instance_bboxes(mesh, instance_ids, objectId2labelId):
     instance_bboxes = np.zeros((num_instances, 8)) # (cx, cy, cz, dx, dy, dz, ins_label, objectId)
     for objectId in objectId2labelId:
         ins_label = objectId2labelId[objectId] # nyu40id
-        obj_pc = mesh[instance_ids==objectId, 0:3] # 
+        obj_pc = mesh[instance_ids==objectId, 0:3] #
         if len(obj_pc) == 0: continue
         xmin = np.min(obj_pc[:,0])
         ymin = np.min(obj_pc[:,1])
@@ -130,7 +129,7 @@ def get_instance_bboxes(mesh, instance_ids, objectId2labelId):
         xmax = np.max(obj_pc[:,0])
         ymax = np.max(obj_pc[:,1])
         zmax = np.max(obj_pc[:,2])
-        bbox = np.array([(xmin+xmax)/2, (ymin+ymax)/2, (zmin+zmax)/2, xmax-xmin, ymax-ymin, zmax-zmin, ins_label, objectId]) 
+        bbox = np.array([(xmin+xmax)/2, (ymin+ymax)/2, (zmin+zmax)/2, xmax-xmin, ymax-ymin, zmax-zmin, ins_label, objectId])
         instance_bboxes[objectId,:] = bbox
     return instance_bboxes
 
@@ -149,12 +148,9 @@ def export(scene, cfg):
     axis_align_matrix = read_axis_align_matrix(meta_file)
     aligned_mesh = align_mesh_vertices(mesh, axis_align_matrix) #（num_verts, 9) aligned_xyz+rgb+normal
 
-    # mesh[:, :3] -= mesh[:, :3].mean(0) # substract xyz mean #TODO: should move to dataset if necessary
-    # aligned_mesh[:, :3] -= aligned_mesh[:, :3].mean(0) # substract aligned xyz mean
-
     if os.path.isfile(agg_file):
         # read label_file
-        sem_labels = read_label_file(label_file) 
+        sem_labels = read_label_file(label_file)
         # read seg_file
         seg2verts, num = read_seg_file(seg_file)
         assert num_verts == num
@@ -168,7 +164,7 @@ def export(scene, cfg):
         aligned_instance_bboxes = get_instance_bboxes(aligned_mesh, instance_ids, objectId2labelId)
     else:
         # use zero as placeholders for the test scene
-        print("use placeholders")
+        # print("use placeholders")
         sem_labels = np.zeros(shape=(num_verts), dtype=np.uint32) # 0: unannotated
         instance_ids = np.ones(shape=(num_verts), dtype=np.uint32) * -1 # -1: unannotated
         instance_bboxes = np.zeros((1, 8))
@@ -179,44 +175,28 @@ def export(scene, cfg):
 
 def process_one_scan(scan, cfg):
     mesh, aligned_mesh, sem_labels, instance_ids, instance_bboxes, aligned_instance_bboxes = export(scan, cfg)
-    # print('Num of instances: ', len(np.unique(instance_ids)))
     sem_labels = remapper[sem_labels] # nyu40id -> {0, 1, ..., 19}
 
     if instance_bboxes.shape[0] > 1:
         num_instances = len(np.unique(instance_ids))
-        # print('Num of instances: ', num_instances)
-
         bbox_mask = np.logical_not(np.in1d(instance_bboxes[:,-2], DONOTCARE_CLASS_IDS)) # match the mesh2cap; not care wall, floor and ceiling for instances
         instance_bboxes = instance_bboxes[bbox_mask,:]
         aligned_instance_bboxes = aligned_instance_bboxes[bbox_mask,:]
-        print('Num of care instances: ', instance_bboxes.shape[0])
-    else:
-        print("No semantic/instance annotation for test scenes")
+        # print('Num of care instances: ', instance_bboxes.shape[0])
+    # else:
+        # print("No semantic/instance annotation for test scenes")
 
 
     torch.save({'mesh': mesh, 'aligned_mesh': aligned_mesh, 'sem_labels': sem_labels, 'instance_ids': instance_ids, 'instance_bboxes': instance_bboxes, 'aligned_instance_bboxes': aligned_instance_bboxes}, os.path.join(cfg.SCANNETV2_PATH.splited_data, cfg.split, scan+'.pth'))
-    
-    
-def process_all_scans(cfg):
-    SCAN_NAMES = sorted([line.rstrip() for line in open(f'meta_data/scannetv2_{cfg.split}.txt')])     
-    
-    for scan in SCAN_NAMES:
-        print(scan)
-        process_one_scan(scan, cfg)
 
 
-if __name__=='__main__':    
+if __name__=='__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--id', help='scan id', required=True)
     parser.add_argument('-s', '--split', help='data split (train / val / test)', default='train')
-    parser.add_argument('-c', '--cfg', help='scannet configuration YAML file', default='../../conf/path.yaml')
+    parser.add_argument('-c', '--cfg', help='configuration YAML file', default='../../conf/path.yaml')
     opt = parser.parse_args()
-
     cfg = OmegaConf.load(opt.cfg)
     cfg.split = opt.split
-    
     os.makedirs(os.path.join(cfg.SCANNETV2_PATH.splited_data, cfg.split), exist_ok=True)
-
-    # process_one_scan('scene0217_00', cfg)
-
-    print(f'data split: {cfg.split}')
-    process_all_scans(cfg)
+    process_one_scan(opt.id, cfg)
