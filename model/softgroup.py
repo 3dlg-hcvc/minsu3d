dd649c49a11ea011238a71cba5e986be8543aacc
eamonn-zh <eamonn0223@gmail.com>
2022-07-21 16:48:58 -0700

    fix wrong dtype in loss

--- model/softgroup.py
+++ model/softgroup.py
@@ -195,7 +195,7 @@ class SoftGroup(pl.LightningModule):
             labels = fg_instance_cls.new_full((fg_ious_on_cluster.size(0),), self.instance_classes)
             labels[pos_inds] = fg_instance_cls[pos_gt_inds]
             classification_criterion = ClassificationLoss()
-            classification_loss = classification_criterion(output_dict["cls_scores"], labels)
+            classification_loss = classification_criterion(output_dict["cls_scores"], labels.long())
             losses["classification_loss"] = classification_loss
 
             """mask scoring loss"""
