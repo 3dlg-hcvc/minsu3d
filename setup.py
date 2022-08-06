340430a8e481b38dd58925ac524e530054894c04
qiruiw <bitwqr@gmail.com>
2022-08-05 21:13:20 -0700

    clean code to be released

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="minsu3d",
     version="1.0",
     author="3dlg-hcvc",
-    url="https://github.com/3dlg-hcvc/pointgroup-minkowski.git",
+    url="https://github.com/3dlg-hcvc/minsu3d-internal.git",
     description="",
     packages=find_packages(include=("lib", "model")),
     install_requires=["plyfile", "tqdm", "trimesh", "pytorch-lightning==1.6.5", "scipy", "open3d", "wandb", "hydra-core", "h5py"]
