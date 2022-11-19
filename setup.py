acaf4cf389379b11d7a527ed83117104c667e372
Yiming Zhang <49868620+eamonn-zh@users.noreply.github.com>
2022-11-19 02:29:27 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     name="minsu3d",
     version="1.0",
     author="3dlg-hcvc",
-    url="https://github.com/3dlg-hcvc/minsu3d-internal.git",
+    url="https://github.com/3dlg-hcvc/minsu3d.git",
     description="",
-    packages=find_packages(include=("lib", "model")),
+    packages=find_packages(include=("data", "model")),
     install_requires=["plyfile", "tqdm", "trimesh", "pytorch-lightning==1.6.5", "scipy", "open3d", "wandb", "hydra-core", "h5py", "ninja"]
 )
